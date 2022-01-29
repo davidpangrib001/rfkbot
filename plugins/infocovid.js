65bd054ae4c1c907a1469da303d0860a325728db
davidpangrib001 <96653161+davidpangrib001@users.noreply.github.com>
2022-01-29 14:59:54 +0700

    Update infocovid.js

--- plugins/infocovid.js
+++ plugins/infocovid.js
@@ -4,16 +4,18 @@ await m.reply('Searching...')
 /*let covid = await (await fetch('https://i.ibb.co/8db5c7S/Coronavirus-2.jpg')).buffer()
 let res = await (await fetch(`https://api.kawalcorona.com/indonesia/`)).json()
 let hasil = `*INFO COVID-19 INONESIA*
-Positif : ${res.positif}
-Meninggal : ${res.meninggal}
-Dirawat : ${res.dirawat}
-Sembuh : ${res.sembuh}
+Positif 🦠 : ${res.positif}
+Meninggal ⚰️ : ${res.meninggal}
+Dirawat 🏥 : ${res.dirawat}
+Sembuh ➕ : ${res.sembuh}
 Tetap Jaga Kesehatan, Pakai Masker, Stay At Home :3`
 conn.sendFile(m.chat, covid, 'infocovid.jpg', hasil, m)
 */let fetch = require('node-fetch')
 let res = await fetch(`https://api.kawalcorona.com/indonesia`)
-if (!res.ok) throw await res.text()
-let json = await res.json()
+if (!res.ok) throw await res.text()
+
+let json = await res.json()
+
 if (!json.status) throw json
 }
 handler.help = ['infocovid']
